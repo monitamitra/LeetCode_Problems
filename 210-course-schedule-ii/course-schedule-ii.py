e1d198c5ac77692aa063a055559bcc62c62e0c94
Monita Mitra <31115172+monitamitra@users.noreply.github.com>
2024-08-17 15:40:06 -0500

    Time: 93 ms (41.67%) | Memory: 18.3 MB (59.94%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,19 +1,27 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         res = []
+
+        # build graph to default as list for missing keys 
+        # course = node in graph, edge from prereq to course 
+        # making adjcency list 
         graph = defaultdict(list)
         for course, prereq in prerequisites:
             graph[prereq].append(course)
         
+        # calculate incoming edges of a node 
+        # => count number of prereqs for each course
         in_degree = [0] * numCourses
         for course, prereq in prerequisites:
             in_degree[course] += 1
         
+        # courses with no prereqs can be taken first 
         queue = deque()
         for i in range(numCourses):
             if in_degree[i] == 0:
                 queue.append(i)
         
+        # pop course off queue and then check graph for prereqs until 0 remaining
         while queue:
             node = queue.popleft()
             res.append(node)
