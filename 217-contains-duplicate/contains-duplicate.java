9399b86b11e4fff1fb70c23fcff14452014a70d7
Monita Mitra <31115172+monitamitra@users.noreply.github.com>
2024-09-13 19:12:42 -0500

    Time: 15 ms (41.72%) | Memory: 58.3 MB (36.54%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (freq.get(nums[i]) == null) {
+                freq.put(nums[i], 1);
+            } else if (freq.get(nums[i]) == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
