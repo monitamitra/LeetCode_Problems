14fcee21a9cf3c95e537ecdb27bd65a0db831f85
Monita Mitra <31115172+monitamitra@users.noreply.github.com>
2024-10-03 22:10:32 -0500

    Time: 0 ms (100.00%) | Memory: 40.7 MB (83.33%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -18,11 +18,15 @@ class Solution {
         if (p == null && q == null) {
             return true;
         }
-        
-        if (p == null || q == null || p.val != q.val) {
+
+        if (p == null || q == null) {
             return false;
         }
 
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
     }
 }
