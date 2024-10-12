10fff9fd725548bcb36619794abd0ccbd59979c7
Monita Mitra <31115172+monitamitra@users.noreply.github.com>
2024-10-12 15:44:33 -0500

    Time: 6 ms (60.17%) | Memory: 45.8 MB (22.22%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,39 +1,46 @@
 class Solution {
+    HashMap<Integer, ArrayList<Integer>> premap = new HashMap<>();
+    // make set to track all courses along curr DFS path
+    HashSet<Integer> visited = new HashSet<>();
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adjList = new ArrayList<>();
-        int[] inDegree = new int[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            adjList.add(new ArrayList<>());
+            premap.put(i, new ArrayList<>());
+        }
+        for (int[] temp : prerequisites) {
+            // append each prereq of course to course in map
+            int course = temp[0];
+            int pre = temp[1];
+            premap.get(course).add(pre);
         }
 
-        for (int[] prereq : prerequisites) {
-            int course = prereq[0];
-            int curr_req = prereq[1];
-            adjList.get(curr_req).add(course);
-            // num prereqs course has
-            inDegree[course]++;
-        }
-        Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) {
-                // course with no prereqs 
-                q.add(i);
+            if (!dfs(i)) {
+                return false;
             }
+        }
+        return true;
+    }
+
+    private boolean dfs(int curr_course) {
+        // cycle detected, courses can't be completed 
+        if (visited.contains(curr_course)) {
+            return false;
         }
 
-        int count = 0;
-        while (!q.isEmpty()) {
-            int curr = q.poll();
-            count++;
-            // get prereqs for course
-            for (int neighbor : adjList.get(curr)) {
-                inDegree[neighbor]--;
-                if (inDegree[neighbor] == 0) {
-                    // course has no more prereqs
-                    q.add(neighbor);
-                }
+        // course has not prereqs, can be completed
+        if (premap.get(curr_course).size() == 0) {
+            return true;
+        }
+
+        visited.add(curr_course);
+        for (int prereq : premap.get(curr_course)) {
+            if (!dfs(prereq)) {
+                return false;
             }
         }
-        return count == numCourses;
+        visited.remove(curr_course);
+        premap.get(curr_course).clear();
+        return true;
     }
 }
