bbcc23409af580977892a6cf986f98accd3052b6
Monita Mitra <31115172+monitamitra@users.noreply.github.com>
2024-11-06 20:38:46 -0600

    Time: 24 ms (23.29%) | Memory: 45.3 MB (24.23%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -5,11 +5,15 @@ class Solution {
         for (int i = s.length() - 1; i >= 0; i--) {
             for (int j = i; j < s.length(); j++) {
                 if (s.charAt(i) == s.charAt(j)) {
-                    if (j - i <= 1) {
+                    if (i == j) {
+                        // substring with 1 char is always palindrome
                         dp[i][j] = true;
-                        ans++;
-                    } else if (dp[i + 1][j - 1]) {
-                        dp[i][j] = true;
+                    } else if (j == i + 1) {
+                        dp[i][j] = (s.charAt(i) == s.charAt(j));
+                    } else {
+                        dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
+                    }
+                    if (dp[i][j]) {
                         ans++;
                     }
                 }
