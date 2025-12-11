ebaf50cec44c2649e1f931e09581570f0eb79509
Monita Mitra <31115172+monitamitra@users.noreply.github.com>
2025-12-10 18:44:27 -0600

    Time: 108 ms (68.78%) | Memory: 75.4 MB (7.27%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,29 +1,31 @@
 class TimeMap:
 
     def __init__(self):
-        self.store = {} # key : [list of [value, timestamp]]
+        self.map = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.store:
-            self.store[key] = []
-        self.store[key].append([value, timestamp])
+        if key not in self.map:
+            self.map[key] = []
+        
+        self.map[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
         res = ""
-        values = self.store.get(key, [])
-
-        # binary search
-        l, r = 0, len(values) - 1
-        while l <= r:
-            m = (l + r) // 2
-            if values[m][1] <= timestamp:
-                res = values[m][0]
-                l = m + 1
-            else:
-                r = m - 1
+        values = self.map.get(key, [])
+        left, right = 0, len(values) - 1
         
-        return res
+        while left <= right:
+            mid = (left + right) // 2
+            mid_val = values[mid]
+            
+            if mid_val[1] == timestamp:
+                return mid_val[0]
 
+            if mid_val[1] < timestamp:
+                res = mid_val[0]
+                left = mid + 1
+            elif mid_val[1] > timestamp:
+                right = mid - 1
             
 
         return res
