eebb13ec1d47d2665bd99d7cb5d9aa7272a963b6
Monita Mitra <31115172+monitamitra@users.noreply.github.com>
2025-12-14 15:59:50 -0600

    Time: 0 ms (100.00%) | Memory: 18.2 MB (41.14%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,24 +1,27 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
+
+        # try to find pivot index
         while l <= r:
             m = (l + r) // 2
-            if nums[m] == target:
+            # found target 
+            if target == nums[m]:
                 return m
-            
-            # left sorted portion
-            if nums[m] >= nums[l]:
+
+            # left side is sorted
+            if nums[l] <= nums[m]:
+                # target is not in sorted portion
                 if target > nums[m] or target < nums[l]:
-                    # need to search right
                     l = m + 1
                 else:
                     r = m - 1
-            # right sorted portion
             else:
+                # right side is sorted
+                # ex: [7, 0, 1, 2, 4, 5, 6]
                 if target < nums[m] or target > nums[r]:
                     r = m - 1
                 else:
                     l = m + 1
-            
-
+         
         return -1
