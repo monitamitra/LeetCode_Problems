df34b7677cf730b3e6ea54846305d7ff28371f44
Monita Mitra <31115172+monitamitra@users.noreply.github.com>
2025-12-14 22:09:02 -0600

    Time: 0 ms (100.00%) | Memory: 17.8 MB (64.71%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -14,3 +14,17 @@ class Solution:
 
         return dp[m - 1][n - 1]
 
+
+# Only store one row (the "previous" row)
+    # Initialize with 1s because the top row is always 1
+   #  row = [1] * n
+    
+    # Iterate through the grid rows (skipping the first one)
+    # for i in range(1, m):
+    #     for j in range(1, n):
+            # The Magic:
+            # row[j] starts as the value from the "top" (old value)
+            # row[j-1] is the value from the "left" (newly updated value)
+    #         row[j] = row[j] + row[j-1]
+            
+    # return row[-1]
