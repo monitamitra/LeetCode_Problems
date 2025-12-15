a31fd49bfbe6b052589cac332cc5d53731bdd990
Monita Mitra <31115172+monitamitra@users.noreply.github.com>
2025-12-14 21:51:26 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (25.43%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,16 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * n for _ in range(m)]
+        # base case -> don't have top/left neighbor
+        for i in range(m):
+            dp[i][0] = 1
+        for i in range(n):
+            dp[0][i] = 1
+
+        # 0 is handled in base cases
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        return dp[m - 1][n - 1]
+
