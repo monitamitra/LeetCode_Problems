5b5d42a9b5a58f6f339bff972b7ab03bd6419b2a
Monita Mitra <31115172+monitamitra@users.noreply.github.com>
2025-12-18 19:25:17 -0600

    Time: 61 ms (63.86%) | Memory: 18.2 MB (99.95%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.py
+++ 729-my-calendar-i/my-calendar-i.py
@@ -4,14 +4,34 @@ class MyCalendar:
         self.calendar = []
 
     def book(self, startTime: int, endTime: int) -> bool:
-        self.calendar.sort(key=lambda x: x[0])
-        for start, end in self.calendar:
-            if startTime < end and endTime > start:
+        if not self.calendar:
+            self.calendar.append([startTime, endTime])
+            return True
+        
+        l, r = 0, len(self.calendar)
+        while l < r:
+            m = (l + r) // 2
+            curr_interval = self.calendar[m]
+
+            if curr_interval[0] < startTime:
+                l = m + 1
+            else:
+                r = m 
+        
+        # l is index to insert at
+        if l < len(self.calendar):
+            # does right neighbor start before I end?
+            if self.calendar[l][0] < endTime:
+                return False
+        
+        if l > 0:
+            # does left neighbor end after I start?
+            if self.calendar[l-1][1] > startTime:
                 return False
 
-        self.calendar.append([startTime, endTime])
+        self.calendar.insert(l, [startTime, endTime])
         return True
-
+    
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
